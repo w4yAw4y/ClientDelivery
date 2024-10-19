376d10297f3f9cf77261e4656998f3aace7acee4
w4yAw4y <w4yAw4y@yandex.ru>
2024-10-19 16:33:49 +0300

    Прогресс

---
+++ Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientDelivery.Models
+{
+    public class Order
+    {
+        public enum ContainerTypeEnum
+        {
+            Навалочные,
+            Насыпные,
+            Штучные,
+            Наливные,
+            Скоропортящиеся,
+            Негабаритные,
+            Опасные
+        }
+        public enum StatusOrderEnum
+        {
+            Новая,
+            ПереданоНаВыполнение,
+            Выполнена,
+            Отменена
+        }
+        public int Id { get; set; }
+        public string ClientFIO { get; set; }
+        public ContainerTypeEnum ContainerType { get; set; }
+        public string PlaceToPickUpOrder { get; set; }
+        public DateTime TimeToPickUpOrder { get; set; }
+        public string PlaceToDeliveryOrder {  get; set; }
+        public StatusOrderEnum statusOrder { get; set; }
+
+    }
+}
---
+++ Models/OrderManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientDelivery.Models
+{
+    public class OrderManager
+    {
+        public static ObservableCollection<Order> _DatabaseOrder = new ObservableCollection<Order>()
+        {
+            new Order() {
+                Id = 1,
+                ClientFIO = "Аржанов Андрей",
+                ContainerType = Order.ContainerTypeEnum.Скоропортящиеся,
+                PlaceToPickUpOrder = "г. Москва, ул. Добролюбова, д.1, стр. 1",
+                TimeToPickUpOrder = new DateTime (2024, 10,20, 12,30,00),
+                PlaceToDeliveryOrder = "г. Москва, ул. Лескова, д. 30, кв. 11 ",
+                statusOrder = Order.StatusOrderEnum.Новая
+            }, new Order()
+            {
+                Id = 2,
+                ClientFIO = "Аржанова Анна",
+                ContainerType = Order.ContainerTypeEnum.Опасные,
+                PlaceToPickUpOrder = "г. Москва, ул. Добролюбова, д.2, стр. 1",
+                TimeToPickUpOrder = new DateTime (2024, 10,10, 10,10,00),
+                PlaceToDeliveryOrder = "г. Москва, ул. Лескова, д. 30, кв. 12 ",
+                statusOrder = Order.StatusOrderEnum.Выполнена
+            }
+        };
+
+        public static ObservableCollection<Order> GetOrder()
+        {
+            return _DatabaseOrder;
+        }
+
+        public static void AddUser(Order order)
+        {
+            _DatabaseOrder.Add(order);
+        }
+
+        public static void DeleteUser(Order order)
+        {
+            _DatabaseOrder.Remove(order);
+        }
+    }
+}
